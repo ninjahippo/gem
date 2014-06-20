9d104336b250a1d71966ffd88fe10addd2da2aea
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-19 21:02:20 -0500

    bug fix

--- lib/generators/ninjahippo/templates/ninjahippo_controller.rb
+++ lib/generators/ninjahippo/templates/ninjahippo_controller.rb
@@ -3,14 +3,14 @@ require 'json'
 
 class NinjahippoController < ApplicationController
   def index
-    @html = JSON.parse(Net::HTTP.get_response(URI.parse(NH_BASE + 'sites/' + APP_NAME + '/pages/' + 'index' + '?application_token=' + APP_TOKEN)).body)[0]['html']
+    @html = JSON.parse(Net::HTTP.get_response(URI.parse(NH_BASE + 'sites/' + APP_NAME + '/pages/' + 'index' + '?application_token=' + APP_TOKEN)).body)['html']
     render 'show'
   end
 
   def show
     nh = Net::HTTP.get_response(URI.parse(NH_BASE + 'sites/' + APP_NAME + '/pages/' + params[:slug] + '?application_token=' + APP_TOKEN))
     if nh.code != '401'
-      @html = JSON.parse(nh.body)[0]['html']
+      @html = JSON.parse(nh.body)['html']
     else
       render :file => 'public/404.html', :status => :not_found, :layout => false
     end
