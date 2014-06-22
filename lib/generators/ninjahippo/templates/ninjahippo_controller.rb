3492e5d8f7bd627406dd4ea7388c0c29fd643a6f
Joah Gerstenberg <joahd@gbergfamily.com>
2014-06-21 20:52:48 -0500

    slimmed down the controller a bit

--- lib/generators/ninjahippo/templates/ninjahippo_controller.rb
+++ lib/generators/ninjahippo/templates/ninjahippo_controller.rb
@@ -2,15 +2,11 @@ require 'net/http'
 require 'json'
 
 class NinjahippoController < ApplicationController
-  def index
-    @html = JSON.parse(Net::HTTP.get_response(URI.parse(NH_BASE + 'sites/' + APP_NAME + '/pages/' + 'index' + '?application_token=' + APP_TOKEN)).body)['html']
-    render 'show'
-  end
-
   def show
-    nh = Net::HTTP.get_response(URI.parse(NH_BASE + 'sites/' + APP_NAME + '/pages/' + params[:slug] + '?application_token=' + APP_TOKEN))
-    if nh.code != '401'
-      @html = JSON.parse(nh.body)['html']
+    @slug = params[:slug] || 'index'
+    @nh = Net::HTTP.get_response(URI.parse(NH_BASE + 'sites/' + APP_NAME + '/pages/' + @slug + '?application_token=' + APP_TOKEN))
+    if @nh.code != '401' and params[:slug] != 'index'
+      @html = JSON.parse(@nh.body)['html']
     else
       render :file => 'public/404.html', :status => :not_found, :layout => false
     end
